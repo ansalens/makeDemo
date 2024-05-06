db9eb79a090095ee9a2f7fc3a806eab6dc7d1556
Sale <777aleksandarnikolic@gmail.com>
2024-05-06 15:55:39 +0200

    Fix broken code

--- hellofunc.c
+++ hellofunc.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <hellomake.h>
+#include "hellomake.h"
 
 void myPrintHelloMake(void) {
-
-  printf("Hello makefiles!\n");
-
-  return;
+  printf("****\tHello ansalens!\t****\n");
 }
